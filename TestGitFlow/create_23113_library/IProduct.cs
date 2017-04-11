06c578f130d7a97cbb4f96b1143980cce31d7ce4
Serhii <semenov@mksat.net>
2017-04-11 20:51:41 +0300

    IProduct is release

---
+++ TestGitFlow/create_23113_library/IProduct.cs
@@ -0,0 +1,9 @@
+﻿namespace create_23113_library
+{
+    public interface IProduct
+    {
+        int Id { get; }
+        int Price { get;  }
+        Categories Category { get; }
+    }
+}
---
+++ TestGitFlow/create_23113_library/Smartphone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_23113_library
+{
+    public class Smartphone : IProduct
+    {
+        public int Id
+        {
+            get { return Id; }
+            protected set { Id = value; }
+        }
+
+        public int Price
+        {
+            get { return Price; }
+            protected set { Price = value; }
+        }
+
+        public Categories Category
+        {
+            get { return Category; }
+            protected set { Category = value; }
+        }
+    }
+}
