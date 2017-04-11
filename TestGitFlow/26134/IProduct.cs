377975ef9c813194b787ded8a4a765b523ad6eb5
unknown <26134@L7A-02.step.edu>
2017-04-11 20:56:41 +0300

    Add Product

---
+++ TestGitFlow/26134/IProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26134
+{
+    interface IProduct
+    {
+    }
+}
