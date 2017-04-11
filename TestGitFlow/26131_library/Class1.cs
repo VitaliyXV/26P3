54a447f757a4030d976c9c6459ba3a8808a51896
Diana <didi77777@ukr.net>
2017-04-11 19:44:47 +0300

    create 26131 library

---
+++ TestGitFlow/26131_library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26131_library
+{
+    public class Class1
+    {
+    }
+}
