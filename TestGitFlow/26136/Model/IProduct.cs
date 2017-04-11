01b3128e27b2b4edc316cf697e9588b7048185a3
Laptin <laptinivan10@gmail.com>
2017-04-11 20:50:12 +0300

    Add interface IPorudct and 2 class: Phone,Computer

---
+++ TestGitFlow/26136/Model/IProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26136.Model
+{
+    public enum Category
+    {
+        Apple,
+        Samsung,
+        Vertu,
+        Nokia,
+        Meizu
+    }
+
+    interface IProduct 
+    {
+        int Id { get; }
+        int Price { get; }
+        Category Type { get; }
+    }
+}
