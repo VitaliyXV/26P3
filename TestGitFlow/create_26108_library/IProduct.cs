83161835f0bd559f22ddada00273b4b51ce79244
Mirzaf <26108@step.edu>
2017-04-11 20:46:38 +0300

    fail change

---
+++ TestGitFlow/create_26108_library/IProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_26108_library
+{
+    interface IProduct
+    {
+        int Id { get; set; }
+        int Price { get; set; }
+        enum Categoryes
+        { 
+            Phone,
+            Car, 
+            Window,
+            Food,
+            Boots
+        }
+    }
+}
---
+++ TestGitFlow/create_26108_library/Phone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_26108_library
+{
+    public class Phone: IProduct
+    {
+        public int Id
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        
+    }
+}
