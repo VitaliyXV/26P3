283a59ceefc0e0f5ddaaeaa41e775dbc76115842
Шевченко Александр Геннадьевич <26114@step.edu>
2017-04-12 19:16:33 +0300

    fix

--- TestGitFlow/TestGitFlowConsole/Program.cs
+++ TestGitFlow/TestGitFlowConsole/Program.cs
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-﻿using _26114_library;
-=======
-﻿using _26136.Model;
->>>>>>> develop
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,13 +10,6 @@ namespace TestGitFlowConsole
     {
         static void Main(string[] args)
         {
-
-            IProduct phone1 = new Phone(1, 100.1, type.aukva);
-            IProduct phone2 = new Phone(2, 120.1, type.black);
-            IProduct phone3 = new Phone(3, 130.1, type.dual_sim);
-            IProduct phone4 = new Phone(4, 150.1, type.phone);
-            IProduct phone5 = new Phone(5, 160.1, type.smartfon);
-
         }
     }
 }
