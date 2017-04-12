85e1c68b1d342bde2801d57fb4a4ea7da8f419b7
Artem <arty7731@gmail.com>
2017-04-12 14:54:12 +0300

    Added classes Computer, Keyboard, Notebook

---
+++ TestGitFlow/26139/Computer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26139
+{
+	class Computer : IProduct
+	{
+		public Category Category
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Id
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Price
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
