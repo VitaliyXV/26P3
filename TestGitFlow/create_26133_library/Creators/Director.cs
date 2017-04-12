739188328ef39c6d0e34928f27d5f751021366f2
Roman <roman.tymokhov@gmail.com>
2017-04-12 20:54:25 +0300

    Creat Director

---
+++ TestGitFlow/create_26133_library/Creators/Director.cs
@@ -0,0 +1,44 @@
+﻿using create_26133_library.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_26133_library.Creators
+{
+    class Director
+    {
+        private Director(Devices device, Category category )
+        {
+            switch(device)
+            {
+                case Devices.Camera:
+                    {
+                        IProductFactory product = new CameraCreator();
+                        product.CreateProduct(category);
+                    }; break;
+                case Devices.Computer:
+                    {
+                        IProductFactory product = new ComputerCreator();
+                        product.CreateProduct(category);
+                    }; break;
+                case Devices.Phone:
+                    {
+                        IProductFactory product = new PhoneCreator();
+                        product.CreateProduct(category);
+                    }; break;
+                case Devices.Tablet:
+                    {
+                        IProductFactory product = new TabletCreator();
+                        product.CreateProduct(category);
+                    }; break;
+                case Devices.TV:
+                    {
+                        IProductFactory product = new TVCreator();
+                        product.CreateProduct(category);
+                    }; break;
+            }
+        }
+    }
+}
---
+++ TestGitFlow/create_26133_library/Enums/Devices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_26133_library.Enums
+{
+    enum Devices { TV, Tablet, Phone, Computer, Camera}
+}
