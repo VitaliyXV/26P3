c4d6fb6bb94096ba461a072066e04ac8201e3236
Дворный Денис Викторович <26144@step.edu>
2017-04-12 20:44:05 +0300

    commit before push

--- TestGitFlow/26144/ProductDirector.cs
+++ TestGitFlow/26144/ProductDirector.cs
@@ -8,28 +8,37 @@ namespace _26144
 {
     public class ProductDirector
     {
+        private static Dictionary<Category, ProductCreator> productCreators = new Dictionary<Category, ProductCreator>();
         public static IProduct CreateProduct(Category category, int id, int price)
         {
             ProductCreator creator = null;
-            switch (category)
+            if (productCreators.ContainsKey(category))
             {
-                case Category.Phone:
-                    creator = new PhoneCreator();
-                    return creator.CreateProduct(id, price);
-                case Category.TV:
-                    creator = new TVCreator();
-                    return creator.CreateProduct(id, price);
-                case Category.Climate:
-                    creator = new ClimateCreator();
-                    return creator.CreateProduct(id, price);
-                case Category.Kitchen:
-                    creator = new KitchenCreator();
-                    return creator.CreateProduct(id, price);
-                case Category.Accessories:
-                    creator = new AccessoriesCreator();
-                    return creator.CreateProduct(id, price);
-                default:
-                    return null;
+                creator = productCreators[category];
+                return creator.CreateProduct(id, price);
+            }
+            else
+            {
+                switch (category)
+                {
+                    case Category.Phone:
+                        creator = new PhoneCreator();
+                        return creator.CreateProduct(id, price);
+                    case Category.TV:
+                        creator = new TVCreator();
+                        return creator.CreateProduct(id, price);
+                    case Category.Climate:
+                        creator = new ClimateCreator();
+                        return creator.CreateProduct(id, price);
+                    case Category.Kitchen:
+                        creator = new KitchenCreator();
+                        return creator.CreateProduct(id, price);
+                    case Category.Accessories:
+                        creator = new AccessoriesCreator();
+                        return creator.CreateProduct(id, price);
+                    default:
+                        return null;
+                }
             }
         }
     }
