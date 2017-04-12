033ea979216670dc8ff97282339fc4ad2ac23714
Дворный Денис Викторович <26144@step.edu>
2017-04-12 20:24:43 +0300

    commit before merge develop branch

---
+++ TestGitFlow/26144/ProductDirector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26144
+{
+    public class ProductDirector
+    {
+        public static IProduct CreateProduct(Category category, int id, int price)
+        {
+            ProductCreator creator = null;
+            switch (category)
+            {
+                case Category.Phone:
+                    creator = new PhoneCreator();
+                    return creator.CreateProduct(id, price);
+                case Category.TV:
+                    creator = new TVCreator();
+                    return creator.CreateProduct(id, price);
+                case Category.Climate:
+                    creator = new ClimateCreator();
+                    return creator.CreateProduct(id, price);
+                case Category.Kitchen:
+                    creator = new KitchenCreator();
+                    return creator.CreateProduct(id, price);
+                case Category.Accessories:
+                    creator = new AccessoriesCreator();
+                    return creator.CreateProduct(id, price);
+                default:
+                    return null;
+            }
+        }
+    }
+}
