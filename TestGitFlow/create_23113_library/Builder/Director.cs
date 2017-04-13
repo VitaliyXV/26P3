c9d9bc17bea13f78619d5f2cfecffa26d746c16a
Serhii-Semenov <semenov!@mksat.net>
2017-04-13 16:26:05 +0300

    no message

--- TestGitFlow/create_23113_library/Builder/Director.cs
+++ TestGitFlow/create_23113_library/Builder/Director.cs
@@ -3,12 +3,21 @@ using System;
 
 namespace create_26113_library.Builder
 {
+    //IProduct prod = Director.CreateProductG<Laptop>();
+    //Console.WriteLine("{0} >> Price : {1} $", prod, prod.Price);
+
+    //prod = Director.CreateProduct(Categories.PC);
+    //Console.WriteLine("{0} >> Price : {1} $", prod, prod.Price);
+
     public class Director
     {
+        static IProduct someProduct = null;
+        static ICreator someCreator = null;
+
         public static IProduct CreateProduct(Categories category)
         {
-            IProduct someProduct = null;
-            ICreator someCreator = null;
+            someProduct = null;
+            someCreator = null;
             switch (category)
             {
                 case Categories.Laptop:
@@ -35,6 +44,44 @@ namespace create_26113_library.Builder
                     throw new Exception("Category is undefined");
                     break;
             }
+            return someProduct;
+        }
+
+        public static IProduct CreateProductG<T>()
+        {
+            someProduct = null;
+            someCreator = null;
+
+            if (typeof(T) == typeof(Laptop))
+            {
+                someCreator = new CreatorLaptop();
+                someProduct = someCreator.CreateProduct();
+            }
+            else
+            if (typeof(T) == typeof(PC))
+            {
+                someCreator = new CreatorPC();
+                someProduct = someCreator.CreateProduct();
+            }
+            else
+            if (typeof(T) == typeof(Phone))
+            {
+                someCreator = new CreatorPhone();
+                someProduct = someCreator.CreateProduct();
+            }
+            else
+            if (typeof(T) == typeof(Smartphone))
+            {
+                someCreator = new CreatorSmartphone();
+                someProduct = someCreator.CreateProduct();
+            }
+            else
+            if (typeof(T) == typeof(Tablet))
+            {
+                someCreator = new CreatorTablet();
+                someProduct = someCreator.CreateProduct();
+            }
+            else throw new Exception("Category is undefined");
             return someProduct;
         }
     }
