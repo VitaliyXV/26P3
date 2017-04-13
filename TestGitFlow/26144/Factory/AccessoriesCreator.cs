c812efa23e69cd6ac46a222a7d1eb7a43d7dd13b
Дворный Денис Викторович <26144@step.edu>
2017-04-13 18:51:40 +0300

    refactored Factory and Model

--- TestGitFlow/26144/Factory/AccessoriesCreator.cs
+++ TestGitFlow/26144/Factory/AccessoriesCreator.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using _26144.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _26144
+namespace _26144.Factory
 {
-    public class PhoneCreator : ProductCreator
+    public class AccessoriesCreator : ProductCreator
     {
         public override IProduct CreateProduct(int id, int price)
         {
-            return new Phone(id, price);
+            return new Accessories(id, price);
         }
     }
 }
