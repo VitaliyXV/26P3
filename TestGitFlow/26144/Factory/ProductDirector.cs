8527eb392ea6e22100995d7b317cb4c24b886a91
Дворный Денис Викторович <26144@step.edu>
2017-04-13 19:18:18 +0300

    added typeCreator

--- TestGitFlow/26144/Factory/ProductDirector.cs
+++ TestGitFlow/26144/Factory/ProductDirector.cs
@@ -10,6 +10,7 @@ namespace _26144.Factory
     public class ProductDirector
     {
         private static Dictionary<Category, ProductCreator> productCreators = new Dictionary<Category, ProductCreator>();
+        private static Dictionary<Type, ProductCreator> productCreatorsT = new Dictionary<Type, ProductCreator>();
 
         public ProductDirector()
         {
@@ -18,6 +19,12 @@ namespace _26144.Factory
 
         private void Initialize()
         {
+            InitializeByCategory();
+            InitializeByType();
+        }
+
+        private void InitializeByCategory()
+        {
             productCreators.Add(Category.Phone, new PhoneCreator());
             productCreators.Add(Category.TV, new TVCreator());
             productCreators.Add(Category.Climate, new ClimateCreator());
@@ -25,9 +32,23 @@ namespace _26144.Factory
             productCreators.Add(Category.Accessories, new AccessoriesCreator());
         }
 
+        private void InitializeByType()
+        {
+            productCreatorsT.Add(typeof(PhoneCreator), new PhoneCreator());
+            productCreatorsT.Add(typeof(TVCreator), new TVCreator());
+            productCreatorsT.Add(typeof(ClimateCreator), new ClimateCreator());
+            productCreatorsT.Add(typeof(KitchenCreator), new KitchenCreator());
+            productCreatorsT.Add(typeof(AccessoriesCreator), new AccessoriesCreator());
+        }
+
         public static IProduct CreateProduct(Category category, int id, int price)
         {
             return productCreators[category].CreateProduct(id, price);
+        }
+
+        public static IProduct CreateProduct<T>(int id, int price)
+        {
+            return productCreatorsT[typeof(T)].CreateProduct(id, price);
         }
     }
 }
