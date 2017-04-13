0229b31e34853336a96195a911db8cbafae7894a
Laptin <laptinivan10@gmail.com>
2017-04-13 19:09:04 +0300

    Chenge Director

--- TestGitFlow/TestGitFlowConsole/Program.cs
+++ TestGitFlow/TestGitFlowConsole/Program.cs
@@ -1,20 +1,17 @@
-﻿using create_26113_library;
-using create_26113_library.Builder;
-using create_26113_library.Factory;
-using System;
-
+﻿using System;
+using _26136;
+using _26136.Model;
+using _26136.Creator;
 namespace TestGitFlowConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IProduct prod = Director.CreateProductG<Laptop>();
-            Console.WriteLine("{0} >> Price : {1} $", prod, prod.Price);
+            //IProduct pr = Director.CreateProduct(CategoryType.Clothing, 1, 15);
 
-            prod = Director.CreateProduct(Categories.PC);
-            Console.WriteLine("{0} >> Price : {1} $", prod, prod.Price);
-
+            IProduct pr = Director.CreateProduct<Sofa>(CategoryType.Clothing, 1, 15);
+            Console.WriteLine(pr.Price);
         }
     }
 }
